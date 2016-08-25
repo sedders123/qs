f01d1cee1c02b3756c25b4d4441ee921eb8b9102
James Seden Smith <sedders123@gmail.com>
2016-08-25 21:03:59 +0100

    Generalised load and create functions.
    Started implementing projects

--- qs/cli.py
+++ qs/cli.py
@@ -6,6 +6,8 @@ from typing import List
 
 APP_DIR = click.get_app_dir("qs") + "/"
 CONFIG_PATH = APP_DIR + "config.json"
+PROJECTS_PATH = APP_DIR + "projects.json"
+
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -20,29 +22,32 @@ class cd:
         os.chdir(self.savedPath)
 
 
-def _load_config() -> None:
-    if _config_exists():
-        with open(CONFIG_PATH, 'r') as f:
-            config = json.load(f)
+def _load_file(file: str) -> None:
+    if _file_exists(file):
+        with open(file, 'r') as f:
+            file_contents = json.load(f)
     else:
-        config = _create_config()
-    return config
+        file_contents = _create_file(file)
+    return file_contents
 
 
-def _config_exists() -> None:
-    if os.path.isfile(CONFIG_PATH):
+def _file_exists(file: str) -> None:
+    if os.path.isfile(file):
         return True
     else:
         return False
 
 
-def _create_config() -> None:
-    config = {'BASE_DIR': os.path.expanduser("~/Projects/")}
+def _create_file(file: str) -> None:
     if not os.path.exists(APP_DIR):
         os.makedirs(APP_DIR)
-    with open(CONFIG_PATH, 'w') as f:
-        json.dump(config, f)
-    return config
+    if file[len(APP_DIR):] == "config.json":
+        file_contents = {'BASE_DIR': os.path.expanduser("~/git/")}
+    elif file[len(APP_DIR):] == "projects.json":
+        file_contents = {}
+    with open(file, 'w') as f:
+        json.dump(file_contents, f)
+    return file_contents
 
 
 def _get_sub_dirs(base_dir: str) -> List[str]:
@@ -57,10 +62,12 @@ def _get_git_repos(dirs: List[str], base_dir: str) -> List[str]:
             repos.append(base_dir + directory)
     return repos
 
+
 def _parse_raw_git_branch(raw_branch: str):
     branch_text = raw_branch.decode("utf-8")
     branch = branch_text[10:-1]
     return branch
+
 
 def _sync_repo(repo: str, base_dir: str):
     repo_name = repo[len(base_dir):]
@@ -84,24 +91,39 @@ def _sync_repos(repos: List[str], base_dir: str):
 def main(ctx):
     """A simple CLI to aid in common, repetitive development tasks"""
     ctx.obj = {}
-    ctx.obj['CONFIG'] = _load_config()
+    ctx.obj['CONFIG'] = _load_file(CONFIG_PATH)
 
 
 @main.command()
 @click.option('--base-dir', '-p', is_flag=True, help='Base Projects Folder')
 def config(ctx, base_dir):
-    config = _load_config()
+    config = _load_file(CONFIG_PATH)
     if base_dir:
         config["BASE_DIR"] = base_dir
 
+
 @main.command()
-@click.pass_context
-def sync(ctx):
+def sync():
     """Syncs all projects in the base directory"""
-    base_dir = ctx.obj['CONFIG']['BASE_DIR']
+    config = _load_file(CONFIG_PATH)
+    base_dir = config['BASE_DIR']
+    click.echo(APP_DIR)
     dirs = _get_sub_dirs(base_dir)
     repos = _get_git_repos(dirs, base_dir)
     _sync_repos(repos, base_dir)
+
+
+@main.group()
+@click.pass_context
+def project(ctx):
+    ctx.obj['PROJECTS'] = _load_file(PROJECTS_PATH)
+
+
+@project.command(name="add")
+@click.argument('project_name')
+def project_add(project_name):
+    click.echo(project_name)
+
 
 @main.command()
 def test():
